238ed8fecde09f1045ba4d0dd2d56eb78384e5f6
thequbit <thequbit@gmail.com>
2013-01-18 21:51:03 -0500

    first commit of shell of the page.  This project is all going to be one page ... here we go!

---
+++ database/create_db.sql
@@ -0,0 +1,74 @@
+create database ideadrop;
+
+grant usage on __database__.* to __username__ identified by '__password__';
+
+grant all on __database__.* to __username__;
+
+use ideadrop;
+
+create table users(
+	userid int not null auto_increment primary key,
+	username varchar(255) not null,
+	passwordhash varchar(255) not null,
+	permissionid int not null,
+	validationcode varchar(255) not null,
+	validated bool not null,
+	profileid int not null
+);
+	
+create table permissions(
+	permissionid int not null auto_increment primary key,
+	enabled bool not null,
+	isadmin bool not null,
+	banned bool not null
+);
+
+create table profiles(
+	profileid int not null auto_increment primary key,
+	profileimageid int not null,
+	userdescription text not null,
+	creationdate date not null,
+	lastlogin datetime not null
+);
+
+create table chops(
+	chopsid int not null auto_increment primary key,
+	chopsname varchar(255),
+	chopsdescription text not null
+);
+
+create table userchops(
+	userchopsid int not null auto_increment primary key,
+	userid int not null,
+	chopsid int not null
+);
+
+create table ideas(
+	ideaid int not null auto_increment primary key,
+	creationdatetime datetime not null,
+	ownerid int not null,
+	name varchar(255) not null,
+	description text not null
+);
+
+create table ideausers(
+	ideauserid int not null auto_increment primary key,
+	ideauser int not null,
+	userid int not null
+);
+
+create table ideachops(
+	ideachopsid int not null auto_increment primary key,
+	ideaid int not null,
+	chopsid int not null
+);
+
+create table workrequests(
+	workrequestid int not null auto_increment primary key,
+	ideaid int not null,
+	userid int not null,
+	requesttext text not null,
+	creationdatetime datetime not null,
+	closeddatetime datetime not null,
+	accepted bool not null 
+);
---
+++ web/index.php
@@ -0,0 +1,96 @@
+<?php
+
+	
+
+?>
+
+<html>
+<head>
+	<title>IdeaDrop - Share Great Ideas</title>
+	
+	<meta name="description" content="Meeting Minute Agrigator and Search Engine for Monroe County, NY">
+	<meta name="keywords" content="Monroe,Minutes,MonroeMinutes,Rochester,Meetings">
+
+	<link href="css/main.css" rel="stylesheet" type="text/css">
+	
+</head>
+<body>
+
+	<div class="top">
+	
+		<div class="sitewrapper">
+		
+			<div class="content">
+			
+				<div class="title">
+					<br>
+					<h1>Idea Drop</h1>
+					<h4> - Share Great Ideas - </h4>
+					<br>
+			
+				</div>
+			
+				<div class="sidebar">
+				
+					<!--
+					<div class="sidebarheader">
+				
+						Welcome to Idea Drop.
+						
+					</div>
+					-->
+					
+					<div class="navlinks">
+					
+						<div class="navlink">
+							<a href="createuser.php">Create Login</a>
+						</div>
+					
+						<div class="navlink">
+							<a href="login.php">User Login</a>
+						</div>
+						
+						<div class="navlink">
+							<a href="createidea.php">Create an Idea</a>
+						</div>
+						
+						<div class="navlink">
+							<a href="searchideas.php">Search For Ideas</a>
+						</div>
+					
+						<div class="navlink">
+							<a href="about.php">About Idea Drop</a>
+						</div>
+					
+					</div>
+				
+				</div>
+				
+				<div class="meat">
+				
+					<!---------------------------------- 
+					--
+					--		PLACE PAGE CONTENT HERE
+					--
+					------------------------------------>
+				
+				</div>
+			
+			</div>
+		
+			<div class="footerwrapper">
+				
+				<div class="footer">
+				
+					Idea Drop - Share Great Ideas | Created by Tim Duffy | 2013 | <a href="https://github.com/thequbit/ideadrop">https://github.com/thequbit/ideadrop</a>
+			
+				</div>
+			
+			</div>
+		
+		</div>
+	
+	</div>
+
+</body>
+</html>
